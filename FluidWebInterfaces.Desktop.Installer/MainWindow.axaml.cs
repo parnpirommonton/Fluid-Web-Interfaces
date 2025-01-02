ef08058fec514b5a7ff47480ab51fc8c056ab3a2
parnpirommonton <parnpirommonton.github@proton.me>
2025-01-02 23:03:08 +0000

    Added FluidWebInterfaces.Desktop.Installer

---
+++ FluidWebInterfaces.Desktop.Installer/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FluidWebInterfaces.Desktop.Installer;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
